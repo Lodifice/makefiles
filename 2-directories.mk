a5918d24f2e5be7c433e8f1e6fbfa44728e0ce8b
Lodifice <Richard.Moerbitz@mailbox.tu-dresden.de>
2016-11-16 10:15:08 +0100

    Minor corrections of second makefile
    Don't push before reading twice :D

--- 2-directories.mk
+++ 2-directories.mk
@@ -9,7 +9,7 @@ OBJDIR	=build
 INCDIR	=inc
 EXE	=spaceinvaders
 # Note: it's funny how I omitted the spaces to close the gaps in the shell
-# output - and screw up readability...
+# output - and screwed up readability...
 
 SRC 	=$(wildcard $(SRCDIR)/*.c)
 DEP	=$(SRC:%.c=%.d)
@@ -23,7 +23,7 @@ CFLAGS	+=-std=c99 ${INC} -Wall -Wextra -Wpedantic -Werror -O3 -D_GNU_SOURCE
 LDFLAGS	+=-lncurses -lpthread -lSDL2
 
 # The VPATH variable is a list of directories. They are searched, whenever a
-# prerequisite isn't found in the current directory. See line 21:
+# prerequisite isn't found in the current directory. See line 65:
 # 	* build/%.o: %.c -> the % might, e.g., be `main`
 # 	* main.c isn't found in the current directory
 # 	* since $(SRCDIR) is part of VPATH, make searches it and finds main.c!
@@ -41,18 +41,16 @@ debug: CFLAGS += -g
 debug: $(EXE)
 
 # | separates normal (left of it) from order-only prerequisites (right of it).
-# Order-only prerequisites are completed (there rules are executed), but they
-# don't serve as a dependency for the target.
-# Thus we make sure that the object directory is created before any object
-# file, but no object file depends on the object directory (which would lead to
-# all object files being rebuild each time we invoke make).
+# Order-only prerequisites are completed (their rules are executed), but they
+# don't serve as a dependency for the target. Think of it as follows:
+# 	* create object file?
+# 	* create build directory first -> updates timestamp of the directory
+# 	* if it was a normal prerequisite, all objects would be	rebuild!
 $(OBJ): | $(OBJDIR)
 
-# This is standard and correctly uses the automatic variables $@ and $^.
 $(EXE): $(OBJ)
 	$(CC) -o $@ $^ $(LDFLAGS)
 
-# Nothing new here either
 -include $(DEP)
 
 # Use sed to rename the `%.o:` in dependency files to `$(OBJDIR)/%.o`.
@@ -67,13 +65,10 @@ $(EXE): $(OBJ)
 build/%.o: %.c
 	$(CC) -c $(CFLAGS) -o $@ $<
 
-# Standard again, though `rm $(OBJ)` is superfluous
 clean:
-	-rm $(EXE) $(OBJ) $(DEP)
+	-rm $(EXE) $(OBJ) $(DEP)	# The $(OBJ) is superfluous...
 	-rm -r $(OBJDIR)
 
 # This finally creates the object directory
 $(OBJDIR):
 	mkdir $@
-
-# Final note: as all this criticism implies, there's a better makefile to come!
